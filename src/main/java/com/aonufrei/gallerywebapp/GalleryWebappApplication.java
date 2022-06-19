38264f49a4af6ac6fb7e8c5f75d9f70246872bc9
Artem Onufrei <onufrei.artem@gmail.com>
2022-06-19 11:14:05 +0300

    Initial commit

---
+++ src/main/java/com/aonufrei/gallerywebapp/GalleryWebappApplication.java
@@ -0,0 +1,13 @@
+package com.aonufrei.gallerywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aonufrei/gallerywebapp/GalleryWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aonufrei.gallerywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
